1730437b4fa7b343475e829f5857e4ed137d8df6
Ganesh Karki <ganeshkarkigk@gmail.com>
2019-03-26 22:47:34 +0900

    Fetch News

--- app/src/main/java/com/classroom/eigonews/MainActivity.java
+++ app/src/main/java/com/classroom/eigonews/MainActivity.java
@@ -3,23 +3,58 @@ package com.classroom.eigonews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private RequestQueue queue;
+    String url = "https://newsapi.org/v2/top-headlines?country=jp&apiKey=".concat(BuildConfig.GOOGLE_NEWS_API_KEY);
+
+    private TextView defaulttextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        defaulttextView = findViewById(R.id.default_text);
+
         queue = Volley.newRequestQueue(this);
 
         Log.d(TAG, "GOOGLE API KEY:" + BuildConfig.GOOGLE_NEWS_API_KEY ); // TODO: Remove this LOG
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        defaulttextView.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                        Log.e(TAG, url);
+                        Log.e(TAG, error.getMessage());
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
+
 }
