804c97044c1289da582384218866c5f7400e4401
Ganesh Karki <ganeshkarkigk@gmail.com>
2019-03-26 21:58:02 +0900

    Use Environment Variable to Inject the Api Key

--- app/src/main/java/com/classroom/eigonews/MainActivity.java
+++ app/src/main/java/com/classroom/eigonews/MainActivity.java
@@ -2,12 +2,24 @@ package com.classroom.eigonews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
+
+        Log.d(TAG, "GOOGLE API KEY:" + BuildConfig.GOOGLE_NEWS_API_KEY ); // TODO: Remove this LOG
+
     }
 }
