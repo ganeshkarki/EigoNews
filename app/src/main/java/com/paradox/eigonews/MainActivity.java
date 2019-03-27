9f3fcc3b8aaf03473be51414c68b57dd94d02feb
Ganesh Karki <ganeshkarkigk@gmail.com>
2019-03-27 23:49:27 +0900

    Add basic vertical page viewer

--- app/src/main/java/com/paradox/eigonews/MainActivity.java
+++ app/src/main/java/com/paradox/eigonews/MainActivity.java
@@ -1,8 +1,15 @@
 package com.paradox.eigonews;
 
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,18 +24,26 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int NUMBER_OF_PAGES = 2;
+
+    MyAdapter mAdapter;
+    VerticalViewPager mPager;
+
     private static final String TAG = "MainActivity";
     private RequestQueue queue;
     String url = "https://newsapi.org/v2/top-headlines?country=jp&apiKey=".concat(BuildConfig.GOOGLE_NEWS_API_KEY);
 
-    private TextView defaulttextView;
+//    private TextView defaulttextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        defaulttextView = findViewById(R.id.default_text);
 
-        defaulttextView = findViewById(R.id.default_text);
+        mAdapter = new MyAdapter(getSupportFragmentManager());
+        mPager = findViewById(R.id.viewpager);
+        mPager.setAdapter(mAdapter);
 
         queue = Volley.newRequestQueue(this);
 
@@ -40,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        defaulttextView.setText("Response: " + response.toString());
+//                        defaulttextView.setText("Response: " + response.toString());
                     }
                 }, new Response.ErrorListener() {
 
@@ -55,6 +70,69 @@ public class MainActivity extends AppCompatActivity {
 
         // Access the RequestQueue through your singleton class.
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+
+    public static class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return NUMBER_OF_PAGES;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    return FragmentOne.newInstance(0, Color.WHITE);
+                case 1:
+                    // return a different Fragment class here
+                    // if you want want a completely different layout
+                    return FragmentOne.newInstance(1, Color.CYAN);
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public static class FragmentOne extends Fragment {
+
+        private static final String MY_NUM_KEY = "num";
+        private static final String MY_COLOR_KEY = "color";
+
+        private int mNum;
+        private int mColor;
+
+        // You can modify the parameters to pass in whatever you want
+        static FragmentOne newInstance(int num, int color) {
+            FragmentOne f = new FragmentOne();
+            Bundle args = new Bundle();
+            args.putInt(MY_NUM_KEY, num);
+            args.putInt(MY_COLOR_KEY, color);
+            f.setArguments(args);
+            return f;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mNum = getArguments() != null ? getArguments().getInt(MY_NUM_KEY) : 0;
+            mColor = getArguments() != null ? getArguments().getInt(MY_COLOR_KEY) : Color.BLACK;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_1, container, false);
+            v.setBackgroundColor(mColor);
+            TextView textView = v.findViewById(R.id.textview);
+            textView.setText("Page " + mNum);
+            return v;
+        }
     }
 
 }
